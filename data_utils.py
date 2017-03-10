b0ceceeecec997003456df8b95b39d982d25eb03
leancz <leancz@gmail.com>
2017-03-10 10:47:43 +0100

    function returns a list in stead of print & bugfix

--- data_utils.py
+++ data_utils.py
@@ -1,15 +1,16 @@
 def dict_compare(d1, d2):
+    output = []
     stack = []
     stack.append(('', d1, d2))
     while len(stack)> 0:
         path, obj1, obj2 = stack.pop()
-        if type(obj1) != type({}):
+        if (type(obj1) != type({})) or (type(obj2) != type({})):
             if obj1 != obj2:
-                print('Value difference at {}: {} != {}'.format(path, obj1, obj2))
+                output.append(('Value difference', path, obj1, obj2))
         else:
             if obj1.keys() != obj2.keys():
-                print('Key difference at {}: {} != {}'.format(path, obj1.keys(), obj2.keys()))
+                output.append(('Key difference', path, obj1.keys(), obj2.keys()))
             else:
                 for i in obj1.keys():
                     stack.append((path + '[' + str(i) + ']', obj1[i], obj2[i]))
-                    
+    return output
